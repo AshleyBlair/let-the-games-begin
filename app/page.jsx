11617818c94b46011d2bda2773c4622dec891130
Ashley Blair <ashleyblairchicago@gmail.com>
2023-07-15 14:41:23 -0500

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+
+const Home = () => {
+    return (
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text text-center'> 
+                Get Together & Play
+                <br className='max-md:hidden'></br>
+                <span className='orange_gradient text-center'> Game Nights with Friends</span>
+            </h1>
+            <p className='desc text-center'>
+                Let the Games Begin is an app for scheduling and sharing game noght to play IRL
+            </p>
+    
+            {/* <Feed /> */}
+        </section>
+      )
+}
+
+export default Home
